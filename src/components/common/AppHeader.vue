cf1fd5b323aa7a23ca9864ae0436005dadca632b
Andrii Sivak <andrii.digiproduct@gmail.com>
2026-04-04 00:45:22 +0200

    feat(header): create header component

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,7 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NotificationsMenu from '@/components/common/NotificationsMenu.vue';
+import UserProfileMenu from '@/components/common/UserProfileMenu.vue';
+</script>
 
 <template>
-    <nav class="app-header navbar navbar-expand bg-body">Header</nav>
-</template>
+    <nav class="app-header navbar navbar-expand bg-body">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                        <i class="bi bi-list"></i>
+                    </a>
+                </li>
+                <li class="nav-item d-none d-md-block">
+                    <router-link to="/dashboard" class="nav-link">Profile</router-link>
+                </li>
+            </ul>
 
-<style scoped></style>
+            <ul class="navbar-nav ms-auto">
+                <NotificationsMenu />
+
+                <UserProfileMenu />
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-lte-toggle="fullscreen">
+                        <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i>
+                        <i
+                            data-lte-icon="minimize"
+                            class="bi bi-fullscreen-exit"
+                            style="display: none"
+                        ></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
