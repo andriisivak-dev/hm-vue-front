44a4217de6f52bb44d514a45281e6d400d884682
Andrii Sivak <andrii.digiproduct@gmail.com>
2026-04-04 14:26:29 +0200

    feat(icons): add SVG icons for total users, customers, and pending reports

--- src/components/SVG/index.ts
+++ src/components/SVG/index.ts
@@ -3,3 +3,6 @@ export { default as IconActivitiesPen } from './IconActivitiesPen.vue';
 export { default as IconActivitiesEye } from './IconActivitiesEye.vue';
 export { default as IconActivitiesCheck } from './IconActivitiesCheck.vue';
 export { default as IconActivitiesThumb } from './IconActivitiesThumb.vue';
+export { default as IconTotalUsers } from './IconTotalUsers.vue';
+export { default as IconTotalCustomers } from './IconTotalCustomers.vue';
+export { default as IconPendingReports } from './IconPendingReports.vue';
